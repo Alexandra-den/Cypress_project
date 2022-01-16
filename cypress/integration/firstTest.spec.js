53ede4a685dda1c1a46bb28543a65f09fd56c335
Alexandra <a.dzianisavets@andersenlab.com>
2022-01-16 12:39:29 +0300

    add comment

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -52,7 +52,7 @@
 // });
 
 
-
+// Comment
 it('Using Get with Find and Eq', () => {
     cy.visit('https://next.privat24.ua/deposit/open')
     cy.get('tbody').find('td').find('div').find('button').eq(1)
